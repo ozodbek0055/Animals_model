213923daee5543990e9fd9868581824b76c4b0fc
ozodbek0055 <ozodbekakramjonov7@gmail.com>
2023-03-31 16:27:30 +0500

    Done

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+st.title("Hayvonlarni klassifikatsiya qiluvchi model")
+
+
+file = st.file_uploader('Rasm yuklash', type = ['png', 'jpeg', 'gif', 'svg', 'jpg'])
+if file:
+    st.image(file)
+
+    img = PILImage.create(file)
+
+    model = load_learner('animals_model.pkl')
+
+    model.predict(img)
+
+    pred, pred_id, prob = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimollik: {prob[pred_id]*100:.1f}%")
+
+    fig = px.bar(x=prob*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
